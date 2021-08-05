60bf4ae1d41e3daa55b4db3d5ddb93d5ec00315b
David.Lacoste <david.lacoste2@gmail.com>
2021-08-05 18:41:23 +0200

    Initial commit

---
+++ src/main/java/io/davlac/tddbddsample/TddBddSample.java
@@ -0,0 +1,24 @@
+package io.davlac.tddbddsample;
+
+import java.util.Arrays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class TddBddSample {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(TddBddSample.class, args);
+
+		System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+		String[] beanNames = ctx.getBeanDefinitionNames();
+		Arrays.sort(beanNames);
+		for (String beanName : beanNames) {
+			System.out.println(beanName);
+		}
+	}
+	
+}
